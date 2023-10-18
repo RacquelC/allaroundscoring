7b082bfb9805d5479b246c6fc079bb328852643a
Bresch90 <92075441+Bresch90@users.noreply.github.com>
2023-10-18 11:34:24 +0200

    Made test for HeptHightJump.

---
+++ src/test/java/heptathlon/HeptHightJumpTest.java
@@ -0,0 +1,105 @@
+package heptathlon;
+
+import common.CalcTrackAndField;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class HeptHightJumpTest {
+
+    private double A = 1.84523;
+    private double B = 75;
+    private double C = 1.348;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void calculateResultCorrect() {
+        double distance = 150.0;
+        CalcTrackAndField calc = new CalcTrackAndField();
+        int expected = calc.calculateField(A, B, C, distance);
+        HeptHightJump heptHightJump = new HeptHightJump();
+
+        // Redirect System.out to a custom PrintStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream customPrintStream = new PrintStream(outputStream);
+        System.setOut(customPrintStream);
+
+        // Call the method that prints to System.out
+        heptHightJump.calculateResult(distance);
+
+        // Restore stream
+        System.setOut(System.out);
+
+        // Verify the printed output
+        assertEquals("The result is: " + expected, outputStream.toString().trim());
+    }
+
+    @Test
+    public void calculateResultLow() {
+        double distance = 75.6;
+        HeptHightJump heptHightJump = new HeptHightJump();
+
+        // Redirect System.out to a custom PrintStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream customPrintStream = new PrintStream(outputStream);
+        System.setOut(customPrintStream);
+
+        // Redefine System.in to a custom ByteStream to escape the method
+        System.setIn(new ByteArrayInputStream("150".getBytes()));
+
+        // Call the method that prints to System.out
+        heptHightJump.calculateResult(distance);
+
+        // Restore streams
+        System.setOut(System.out);
+        System.setIn(System.in);
+
+        // Clean and select output line
+        String[] outputLines = outputStream.toString().split("\n");
+        String actual = outputLines[0].trim();
+
+        // Verify the printed output
+        assertEquals("Value too low", actual);
+    }
+
+    @Test
+    public void calculateResultHigh() {
+        double distance = 270.1;
+        HeptHightJump heptHightJump = new HeptHightJump();
+
+        // Redirect System.out to a custom PrintStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream customPrintStream = new PrintStream(outputStream);
+        System.setOut(customPrintStream);
+
+        // Redefine System.in to a custom ByteStream to escape the method
+        System.setIn(new ByteArrayInputStream("150".getBytes()));
+
+        // Call the method that prints to System.out
+        heptHightJump.calculateResult(distance);
+
+        // Restore streams
+        System.setOut(System.out);
+        System.setIn(System.in);
+
+        // Clean and select output line
+        String[] outputLines = outputStream.toString().split("\n");
+        String actual = outputLines[0].trim();
+
+        // Verify the printed output
+        assertEquals("Value too high", actual);
+    }
+}
